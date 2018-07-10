acc6cbc7eb7cca6432c5e9b3e5695ec6ebd34321
Mikko Seppälä <mikko.v.seppala@gmail.com>
2018-07-11 00:12:17 +0300

    rename main.js to MvsNotesApp.js

--- client/dist/src/index.js
+++ client/dist/src/index.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './style.css';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-//import loginSuccessCallback from './main.js';
-import MvsNotesApp from './main.js';
+import MvsNotesApp from './MvsNotesApp.js';
 
 //loginUser("mikko.v.seppala@gmail.com", "0.juuret", userPool, loginSuccessCallback);
 
