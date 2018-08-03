9dc66d863f05ba73b48be95a189fbdb3a5f4ecc5
Mikko Seppälä <mikko.v.seppala@gmail.com>
2018-08-04 01:35:33 +0300

    fix eslint issues in index.js

--- client/src/index.js
+++ client/src/index.js
@@ -4,16 +4,15 @@ import './style.css';
 import registerServiceWorker from './registerServiceWorker';
 import MvsNotesApp from './MvsNotesApp';
 
-const apiUrl =
-    'https://zvw0ce1n8f.execute-api.eu-central-1.amazonaws.com/dev/mvs-notes/',
+const apiUrl = 'https://zvw0ce1n8f.execute-api.eu-central-1.amazonaws.com/dev/mvs-notes/';
 
-    poolData = {
-        'ClientId': '2hvp9c2sls7f0fq10p8u3t5bt2',
-        'UserPoolId': 'eu-central-1_op50LtdMn'
-    };
+const poolData = {
+  ClientId: '2hvp9c2sls7f0fq10p8u3t5bt2',
+  UserPoolId: 'eu-central-1_op50LtdMn',
+};
 
 ReactDOM.render(
-    <MvsNotesApp apiUrl={apiUrl} poolData={poolData}/>,
-    document.getElementById('root')
+  <MvsNotesApp apiUrl={apiUrl} poolData={poolData}/>,
+  document.getElementById('root')
 );
 registerServiceWorker();
