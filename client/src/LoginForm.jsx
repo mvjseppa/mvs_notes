0617310d516d3c6396254f714b8aa5cca37ae4d2
Mikko Seppälä <mikko.v.seppala@gmail.com>
2018-08-04 11:00:49 +0300

    fix some eslint errors in LoginForm

--- client/src/LoginForm.jsx
+++ client/src/LoginForm.jsx
@@ -1,74 +1,71 @@
 import React from 'react';
-import {AuthenticationDetails, CognitoUser} from 'amazon-cognito-identity-js';
+import { AuthenticationDetails, CognitoUser } from 'amazon-cognito-identity-js';
 
-export default class LoginForm extends React.Component
-{
-    constructor(props) {
-        super(props);
+export default class LoginForm extends React.Component {
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            email: "",
-            passwd: ""
+    this.state = {
+      email: '',
+      passwd: '',
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ [event.target.name]: event.target.value });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    this.loginUser();
+  }
+
+  loginUser() {
+    this.props.requestLoadingPage();
+
+    const cognitoUser = new CognitoUser(
+      { Username: this.state.email, Pool: this.props.userPool },
+    );
+
+    const authDetails = new AuthenticationDetails(
+      { Username: this.state.email, Password: this.state.passwd },
+    );
+
+    const callbacks = {
+      onSuccess: (result) => {
+        this.props.requestNotesPage();
+      },
+
+      onFailure: (err) => {
+        if (err.code === 'UserNotFoundException' || err.code === 'NotAuthorizedException') {
+          this.props.requestLoginPage('Invalid username or password.');
+        } else {
+          this.props.requestLoginPage(err.message);
         }
+      },
 
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
+      mfaRequired: (codeDeliveryDetails) => {
+        const verificationCode = prompt('Please input verification code', '');
+        cognitoUser.sendMFACode(verificationCode, this);
+        this.props.requestLoginPage();
+      },
+    };
 
-    handleChange(event) {
-        this.setState({ [event.target.name]: event.target.value });
-    }
+    cognitoUser.authenticateUser(authDetails, callbacks);
+  }
 
-    handleSubmit(event) {
-        event.preventDefault();
-        this.loginUser();
-    }
-
-    loginUser()
-    {
-        this.props.requestLoadingPage();
-
-        var cognitoUser = new CognitoUser(
-            {Username : this.state.email, Pool : this.props.userPool}
-        );
-
-        var authDetails = new AuthenticationDetails(
-            {Username : this.state.email, Password : this.state.passwd}
-        );
-
-        var callbacks = {
-            onSuccess: (result) => {
-                this.props.requestNotesPage();
-            },
-
-            onFailure: (err) => {
-                if(err.code === "UserNotFoundException" || err.code === "NotAuthorizedException"){
-                    this.props.requestLoginPage("Invalid username or password.");
-                }
-                else{
-                    this.props.requestLoginPage(err.message);
-                }
-            },
-
-            mfaRequired: (codeDeliveryDetails) => {
-                var verificationCode = prompt('Please input verification code' ,'');
-                cognitoUser.sendMFACode(verificationCode, this);
-                this.props.requestLoginPage();
-            }
-        }
-
-        cognitoUser.authenticateUser(authDetails, callbacks);
-    }
-
-    render() {
-        return(
-            <div id="login">
-                <form onSubmit={this.handleSubmit} className="stacked_form">
-                    <input type="text" name="email" onChange={this.handleChange}/>
-                    <input type="password" name="passwd" onChange={this.handleChange}/>
-                    <input type="submit" value="Login"/>
-                </form>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div id="login">
+        <form onSubmit={this.handleSubmit} className="stacked_form">
+          <input type="text" name="email" onChange={this.handleChange} />
+          <input type="password" name="passwd" onChange={this.handleChange} />
+          <input type="submit" value="Login" />
+        </form>
+      </div>
+    );
+  }
 }
