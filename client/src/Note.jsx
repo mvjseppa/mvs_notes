b4dfb3bfeac10805efd8992d218a2363b6b8a8da
Mikko Seppälä <mikko.v.seppala@gmail.com>
2018-08-04 11:45:04 +0300

    fix eslint issues in Note.jsx

--- client/src/Note.jsx
+++ client/src/Note.jsx
@@ -1,69 +1,83 @@
 import React from 'react';
 import $ from 'jquery';
 
-export default class Note extends React.Component{
-    constructor(props) {
-        super(props);
-        this.state = {
-            deleting: false
-        };
-    }
+export default class Note extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      deleting: false,
+    };
+  }
 
-    handleClick() {
-        this.deleteNote(this.props.note_data.id);
-    }
+  handleClick() {
+    this.deleteNote(this.props.note_data.id);
+  }
 
-    deleteNote(id) {
-        this.setState({deleting: true});
+  deleteNote(id) {
+    this.setState({ deleting: true });
 
-        this.props.getToken()
-        .then((token) => {
-            $.ajax({
-                method: 'DELETE',
-                url: this.props.apiUrl+id,
-                headers: { Authorization: token},
-            })
-            .done((result) => {
-                this.props.updateRequest();
-            })
-            .fail((error) => {
-                console.log(JSON.stringify(error));
-                this.props.requestLoginPage();
-            })
-            .always(() => {
-                this.setState({deleting: false});
-            });
+    this.props.getToken()
+      .then((token) => {
+        $.ajax({
+          method: 'DELETE',
+          url: this.props.apiUrl + id,
+          headers: { Authorization: token },
         })
-        .catch((error) => {
+          .done((result) => {
+            this.props.updateRequest();
+          })
+          .fail((error) => {
+            console.log(JSON.stringify(error));
             this.props.requestLoginPage();
-        });
+          })
+          .always(() => {
+            this.setState({ deleting: false });
+          });
+      })
+      .catch((error) => {
+        this.props.requestLoginPage();
+      });
+  }
+
+  render() {
+    let deleteButton = (
+      <button
+        className="note_delete"
+        onClick={this.handleClick.bind(this)}
+        type="button"
+      >
+        &#10005;
+      </button>
+    );
+
+    if (this.state.deleting) {
+      deleteButton = <div className="small_spinner" />;
     }
 
-    render() {
+    const textLines = this.props.note_data.text.split('\n').map(
+      (line, i) => (
+        <p key={i}>
+          {line}
+        </p>
+      ),
+    );
 
-        var deleteButton =
-                <button
-                    className="note_delete"
-                    onClick={this.handleClick.bind(this)}>&#10005;</button>
-
-        if(this.state.deleting){
-            deleteButton = <div className="small_spinner" />
-        }
-
-        var textLines = this.props.note_data.text.split("\n").map(
-            (line, i) => {
-                return ( <p key={i}>{line}</p> );
-            }
-        );
-
-        return(
-            <div className="note" style={{backgroundColor: this.props.note_data.color}}>
-                <div className="note_controls">{deleteButton}</div>
-                <div className="note_contents">{textLines}</div>
-                <div className="tooltip">
-                    <span className="tooltiptext">{this.props.note_data.id}, {this.props.note_data.timestamp}</span>
-                </div>
-            </div>
-        );
-    }
+    return (
+      <div className="note" style={{ backgroundColor: this.props.note_data.color }}>
+        <div className="note_controls">
+          {deleteButton}
+        </div>
+        <div className="note_contents">
+          {textLines}
+        </div>
+        <div className="tooltip">
+          <span className="tooltiptext">
+            {this.props.note_data.id}
+            ,
+            {this.props.note_data.timestamp}
+          </span>
+        </div>
+      </div>
+    );
+  }
 }
