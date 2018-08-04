1ac3cb69af209cc9611b2f933ad4ba29da3b0110
Mikko Seppälä <mikko.v.seppala@gmail.com>
2018-08-05 00:02:11 +0300

    fix eslint issues in NoteCreator and NoteContainer

--- client/src/NoteContainer.jsx
+++ client/src/NoteContainer.jsx
@@ -1,76 +1,78 @@
 import React from 'react';
+import $ from 'jquery';
 import Note from './Note';
 import NoteCreator from './NoteCreator';
-import $ from 'jquery';
 
 export default class NoteContainer extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            loading: true,
-            notes: []
-        };
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: true,
+      notes: [],
+    };
+  }
+
+  componentDidMount() {
+    this.getNotes();
+  }
+
+  getNotes() {
+    if (this.state.notes === []) {
+      this.setState({ loading: true });
     }
 
-    componentDidMount() {
-        this.getNotes();
-    }
-
-    getNotes() {
-        if(this.state.notes===[]){
-            this.setState({loading: true});
-        }
-
-        this.props.getToken()
-        .then((token) => {
-            $.ajax({
-                method: 'GET',
-                url: this.props.apiUrl,
-                headers: { Authorization: token},
-            })
-            .done((result) => {
-                result.sort(function(a, b){return a.timestamp < b.timestamp});
-                this.setState({notes: result});
-            })
-            .fail((error) => {
-                console.log(JSON.stringify(error));
-                this.props.requestLoginPage();
-            })
-            .always(() => {
-                this.setState({loading: false});
-            });
+    this.props.getToken()
+      .then((token) => {
+        $.ajax({
+          method: 'GET',
+          url: this.props.apiUrl,
+          headers: { Authorization: token },
         })
-        .catch((error) => {this.props.requestLoginPage()});
+          .done((result) => {
+            result.sort((a, b) => a.timestamp < b.timestamp);
+            this.setState({ notes: result });
+          })
+          .fail((error) => {
+            console.log(JSON.stringify(error));
+            this.props.requestLoginPage();
+          })
+          .always(() => {
+            this.setState({ loading: false });
+          });
+      })
+      .catch((error) => { this.props.requestLoginPage(); });
+  }
+
+  render() {
+    if (this.state.loading) {
+      return (
+        <div id="user_data">
+          <div className="large_spinner" />
+        </div>
+      );
     }
 
-    render() {
+    const noteElements = this.state.notes.map((note, i) => (
+      <Note
+        key={i}
+        apiUrl={this.props.apiUrl}
+        getToken={this.props.getToken}
+        noteData={note}
+        requestLoginPage={this.props.requestLoginPage}
+        updateRequest={this.getNotes.bind(this)}
+      />
+    ));
 
-        if(this.state.loading){
-            return <div id="user_data"><div className="large_spinner" /></div>
-        }
-
-        var noteElements = this.state.notes.map( (note, i) => {
-            return (
-                <Note
-                    key={i}
-                    apiUrl={this.props.apiUrl}
-                    getToken={this.props.getToken}
-                    noteData={note}
-                    requestLoginPage={this.props.requestLoginPage}
-                    updateRequest={this.getNotes.bind(this)} />
-            );
-        } );
-
-        return (
-            <div id="user_data">
-            <NoteCreator
-                apiUrl={this.props.apiUrl}
-                getToken={this.props.getToken}
-                requestLoginPage={this.props.requestLoginPage}
-                updateRequest={this.getNotes.bind(this)}
-            />
-            { noteElements }
-            </div>
-        );
-    }
+    return (
+      <div id="user_data">
+        <NoteCreator
+          apiUrl={this.props.apiUrl}
+          getToken={this.props.getToken}
+          requestLoginPage={this.props.requestLoginPage}
+          updateRequest={this.getNotes.bind(this)}
+        />
+        { noteElements }
+      </div>
+    );
+  }
 }
--- client/src/NoteCreator.jsx
+++ client/src/NoteCreator.jsx
@@ -1,96 +1,109 @@
 import React from 'react';
 import $ from 'jquery';
 
-export default class NoteCreator extends React.Component{
-    constructor(props) {
-        super(props);
-        this.state = {
-            editing: false,
-            color: '#A0C000',
-            newNote: ""
-        };
+export default class NoteCreator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      editing: false,
+      color: '#A0C000',
+      newNote: '',
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleColorClick = this.handleColorClick.bind(this);
+    this.handleSaveClick = this.handleSaveClick.bind(this);
+    this.handleCancelClick = this.handleCancelClick.bind(this);
+    this.handleCreateNewClick = this.handleCreateNewClick.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ [event.target.name]: event.target.value });
+  }
+
+  handleColorClick(event) {
+    const color = event.target.style.backgroundColor;
+    console.log(event.target.id);
+    console.log(color);
+    this.setState({ color });
+  }
+
+  handleSaveClick(event) {
+    event.preventDefault();
+
+    if (this.state.newNote.length === 0) {
+      return;
     }
 
-    handleChange = event => {
-        this.setState({ [event.target.name]: event.target.value });
+    const noteData = {
+      text: this.state.newNote,
+      color: this.state.color,
+    };
+
+    this.props.getToken()
+      .then((token) => {
+        $.ajax({
+          method: 'POST',
+          url: this.props.apiUrl,
+          headers: { Authorization: token },
+          data: JSON.stringify(noteData),
+        })
+          .done((result) => {
+            this.setState({ newNote: '', editing: false });
+            this.props.updateRequest();
+          })
+          .fail((error) => {
+            console.log(JSON.stringify(error));
+            this.props.requestLoginPage();
+          });
+      })
+      .catch((error) => {
+        this.props.requestLoginPage();
+      });
+  }
+
+  handleCancelClick() {
+    this.setState({ editing: false });
+  }
+
+  handleCreateNewClick() {
+    this.setState({ editing: true });
+  }
+
+  render() {
+    if (!this.state.editing) {
+      return (
+        <div>
+          <button className="new_note_button" type="button" onClick={this.handleCreateNewClick}>
+            +
+          </button>
+        </div>
+      );
     }
 
-    handleColorClick = event => {
-        var color =
-            event.target.style.backgroundColor;
-        console.log(event.target.id);
-        console.log(color);
-        this.setState({ 'color': color });
-    }
+    return (
+      <div className="note" id="note_edit" style={{ backgroundColor: this.state.color }}>
+        <textarea
+          id="new_note"
+          rows="8"
+          cols="32"
+          name="newNote"
+          onChange={this.handleChange}
+          value={this.state.newNote}
+        />
+        <div>
+          <button id="green_button" type="button" style={{ backgroundColor: '#00C0A0' }} className="color_button" onClick={this.handleColorClick} />
+          <button id="yellow_button" type="button" style={{ backgroundColor: '#A0C000' }} className="color_button" onClick={this.handleColorClick} />
+          <button id="red_button" type="button" style={{ backgroundColor: '#C01080' }} className="color_button" onClick={this.handleColorClick} />
 
-    handleSaveClick = event => {
-        event.preventDefault();
-
-        if(this.state.newNote.length === 0){
-            return;
-        }
-
-        var noteData={
-            text: this.state.newNote,
-            color: this.state.color
-        };
-
-         this.props.getToken()
-         .then((token) => {
-             $.ajax({
-                 method: 'POST',
-                 url: this.props.apiUrl,
-                 headers: { Authorization: token},
-                 data: JSON.stringify(noteData),
-             })
-             .done((result) => {
-                 this.setState({newNote: "", editing: false});
-                 this.props.updateRequest();
-             })
-             .fail((error) => {
-                 console.log(JSON.stringify(error));
-                 this.props.requestLoginPage();
-             });
-         })
-         .catch((error) => {
-             this.props.requestLoginPage();
-         });
-    }
-
-    handleCancelClick(){
-        this.setState({editing: false});
-    }
-
-    handleCreateNewClick(){
-        this.setState({editing: true});
-    }
-
-    render() {
-        if(!this.state.editing){
-            return(
-                <div>
-                <button className="new_note_button" onClick={this.handleCreateNewClick.bind(this)}>+</button>
-                </div>
-            );
-        }
-
-        return(
-            <div className="note" id="note_edit" style={{backgroundColor: this.state.color}}>
-                <textarea id="new_note"
-                    rows ="8"
-                    cols ="32"
-                    name="newNote"
-                    onChange={this.handleChange}
-                    value={this.state.newNote}></textarea>
-                <div>
-                    <button id="green_button" style={{backgroundColor: '#00C0A0'}} className="color_button" onClick={this.handleColorClick}/>
-                    <button id="yellow_button" style={{backgroundColor: '#A0C000'}} className="color_button" onClick={this.handleColorClick}/>
-                    <button id="red_button" style={{backgroundColor: '#C01080'}} className="color_button" onClick={this.handleColorClick}/>
-
-                    <button onClick={this.handleSaveClick.bind(this)}>&#10003;</button>
-                    <button onClick={this.handleCancelClick.bind(this)}>&#10005;</button>
-                </div>
-            </div>
-        );
-    }
+          <button type="button" onClick={this.handleSaveClick}>
+            &#10003;
+          </button>
+          <button type="button" onClick={this.handleCancelClick}>
+            &#10005;
+          </button>
+        </div>
+      </div>
+    );
+  }
 }
